f40f0472ecfcbef5805d933f472f4b94c3d81e61
Jason A. Wilson <jasonaaronwilson@gmail.com>
2023-09-07 00:28:23 -0400

    modify the assembler though I still don't like this interface...

--- assembler.c
+++ assembler.c
@@ -9,8 +9,8 @@ typedef struct {
 
 tokenizer_result tokenize(const char *str);
 
-uint64_t assemble(char **statements, uint8_t *memory_start, uint8_t *memory_end,
-                  uint64_t memory_start_address) {
+uint64_t assemble(char **statements, uint8_t *memory_start,
+                  uint64_t memory_length, uint64_t memory_start_address) {
   memory_start[0] = BRK;
   return 1;
 }
--- assembler.h
+++ assembler.h
@@ -5,6 +5,6 @@
 
 // Returns the number of bytes assembled.
 extern uint64_t assemble(char **statements, uint8_t *memory_start,
-                         uint8_t *memory_end, uint64_t memory_start_address);
+                         uint64_t memory_length, uint64_t memory_start_address);
 
 #endif /* _ASSEMBLER_H_ */

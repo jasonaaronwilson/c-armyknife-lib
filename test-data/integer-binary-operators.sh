658f55a36f1b309ec030f11edb0632b78ef0cf64
Jason A. Wilson <jasonaaronwilson@gmail.com>
2023-09-10 19:49:48 -0400

    start think about how to make tests look prettier and easier to write without a bunch of bloat

--- test-data/integer-binary-operators.sh
+++ test-data/integer-binary-operators.sh
@@ -2,48 +2,49 @@
 
 /home/jasonaaronwilson/src/comet-vm/comet-vm <<EOF
 
+# ======================================================================
+# setup
+# ======================================================================
+(*start-assembly*)
+setup:
+imm r1,1
+imm r2,19
+brk
 
-=====================================================================
-========  # Put common immediates in the lower registers.
-========  
-========  setup:
-========  imm r1,1
-========  imm r2 19
-========  brk
-=====================================================================
+test-add:
+add r32,r2,r2
+brk
+(*end-assembly*)
 
+(execute-until-brk 1000 setup test-add)
+(expect-register %r32 38)
 
-=====================================================================
-========  test-add:
-========  add r32,r2,r2
-========  brk
-=====================================================================
-    execute setup test-add
-    expect-register r32 38
+# ======================================================================
+# test_unsigned_sub
+# ======================================================================
+(*start-assembly*)
+test_unsigned_sub:
+sub r32,r2,r2
+nop
+brk
+(*end-assembly*)
+(execute-until-brk 1000 setup test_unsigned_sub)
+(expect-register %r32 38)
+(expect-register %r32 38)
+(expect-register %r32 38)
 
+# ======================================================================
+# test_add
+# ======================================================================
+(*start-assembly*)
+test_add:
+add r32,r2,r2
+brk
+(*end-assembly*)
+(execute setup test_add)
+(expect-register %r32 38)
 
-=====================================================================
-========  test_unsigned_sub:
-========  sub r32,r2,r2
-========  nop
-========  brk
-=====================================================================
-    execute setup test_unsigned_sub
-    expect-register r32 38
-    expect-register r32 38
-    expect-register r32 38
-
-
-======================================================================
-========  test_add:
-========  add r32,r2,r2
-========  brk
-=====================================================================
-    execute setup test_add
-    expect-register r32 38
-
-
-quit
+(quit)
 EOF
 
 EXIT_CODE=$?

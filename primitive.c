f605201b227191c7d2aeb18ee26cec4c1ab4953c
Jason A. Wilson <jasonaaronwilson@gmail.com>
2023-09-13 00:32:10 -0400

    add comet-vm:get-tag (well at least it's definition

--- primitive.c
+++ primitive.c
@@ -40,12 +40,23 @@ tagged_reference_t primtive_function_mul(primitive_arguments_t arguments) {
  * Example (/ 10 2) => 5
  */
 tagged_reference_t primtive_function_div(primitive_arguments_t arguments) {
-  // checkargs macro?
   if (arguments.n_args != 2) {
     fatal_error(ERROR_WRONG_NUMBER_OF_ARGS);
   }
 
   int64_t result
       = untag_int64_t(arguments.args[0]) / untag_int64_t(arguments.args[0]);
+  return tagged_reference(TAG_UINT64_T, result);
+}
+
+/**
+ * comet-vm:get-tag returns the tag number of a scheme object. This is
+ * used to implement primitives like pair? in pure scheme.
+ */
+tagged_reference_t primtive_comet_vm_get_tag(primitive_arguments_t arguments) {
+  if (arguments.n_args != 1) {
+    fatal_error(ERROR_WRONG_NUMBER_OF_ARGS);
+  }
+  uint64_t result = arguments.args[0].tag;
   return tagged_reference(TAG_UINT64_T, result);
 }

fefdbea7fb6fdfd0ea67e145706a38f3cf3ba3b8
Jason A. Wilson <jasonaaronwilson@gmail.com>
2023-09-20 12:28:45 +0200

    a few more string-util testcases

--- tests/string-util-test.c
+++ tests/string-util-test.c
@@ -60,5 +60,36 @@ int main(int argc, char** argv) {
     FAIL("string_contains_char");
   }
 
+  if (string_hash("The quick brown fox") == string_hash("The QUICK brown fox")) {
+    FAIL("string_hash");
+  }
+
+
+  // Memory leak. So what, this is a test...
+  if (!(string_equal(string_substring("The quick brown fox", 4, 9), "quick"))) {
+    FAIL("string_substring");
+  }
+
+  
+  if (string_parse_uint64("0") != 0) {
+    FAIL("string_parse_uint64");
+  }
+  if (string_parse_uint64("1") != 1) {
+    FAIL("string_parse_uint64");
+  }
+  if (string_parse_uint64("0xf") != 15) {
+    FAIL("string_parse_uint64");
+  }
+  if (string_parse_uint64("0b0") != 0) {
+    FAIL("string_parse_uint64");
+  }
+  if (string_parse_uint64("0b1010") != 10) {
+    FAIL("string_parse_uint64");
+  }
+
+  if(!string_duplicate("The quick brown fox")) {
+    FAIL("string_duplicate");
+  }
+
   exit(0);
 }
